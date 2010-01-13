ae2dc2c9f82eb1259acf87e1e20ea8ad9a13c43f
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-01-13 23:12:45 +0000

    Switch to new usb module

---
+++ msd.c
@@ -0,0 +1,23 @@
+#include "msd.h"
+
+
+msd_size_t 
+msd_read (msd_t *msd, msd_addr_t addr, void *buffer, msd_size_t size)
+{
+    return msd->read (msd->handle, addr, buffer, size);
+}
+
+
+msd_size_t
+msd_write (msd_t *msd, msd_addr_t addr, const void *buffer, msd_size_t size)
+{
+    return msd->write (msd->handle, addr, buffer, size);
+
+}
+
+
+msd_status_t
+msd_status_get (msd_t *msd)
+{
+    return msd->status_get (msd->handle);
+}
---
+++ msd.h
@@ -0,0 +1,58 @@
+#ifndef MSD_H
+#define MSD_H
+
+#include "config.h"
+
+typedef uint16_t msd_size_t;
+typedef uint64_t msd_addr_t;
+
+
+#ifndef MSD_BLOCK_SIZE_MAX
+#define MSD_BLOCK_SIZE_MAX 512
+#endif
+
+typedef enum
+{
+    MSD_STATUS_READY,
+    MSD_STATUS_BUSY,
+    MSD_STATUS_NODEVICE
+} msd_status_t;
+
+
+typedef struct
+{
+    unsigned int removable:1;
+    unsigned int reserved:7;
+} msd_flags_t;
+
+
+typedef msd_size_t
+(*msd_read_t)(void *handle, msd_addr_t addr, void *buffer, msd_size_t size);
+
+typedef msd_size_t 
+(*msd_write_t)(void *handle, msd_addr_t addr, const void *buffer, msd_size_t size);
+
+typedef msd_status_t
+(*msd_status_get_t)(void *handle);
+
+
+typedef struct msd_lun_struct
+{
+    void *handle;
+    msd_read_t read;
+    msd_write_t write;
+    msd_status_get_t status_get;
+    msd_addr_t media_bytes;
+    msd_size_t block_bytes;
+    const char *name;
+    msd_flags_t flags;
+} msd_t;
+
+
+msd_size_t msd_read (msd_t *msd, msd_addr_t addr, void *buffer, msd_size_t size);
+
+msd_size_t msd_write (msd_t *msd, msd_addr_t addr, const void *buffer, msd_size_t size);
+
+msd_status_t msd_status_get (msd_t *msd);
+
+#endif
