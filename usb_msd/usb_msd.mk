6301f47269c7b488434d260fe43801a08a36e98a
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-01-26 23:26:51 +0000

    Make naming more consistent

--- usb_msd/usb_msd.mk
+++ usb_msd/usb_msd.mk
@@ -5,7 +5,7 @@ INCLUDES += -I$(USB_MSD_DIR) -I$(USB_MSD_DIR)/../usb/
 
 PERIPHERALS += udp
 
-SRC += usb_bot.c usb_lun.c usb_msd.c usb_msd_dsc.c usb_sbc.c usb.c spi.c
+SRC += usb_msd.c usb_msd_dsc.c usb_msd_sbc.c usb_msd_lun.c usb_bot.c usb.c spi.c
 
 
 
