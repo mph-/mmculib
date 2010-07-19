028daae266fcc85a0238cc851c59f238d47d4a1e
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-07-19 07:31:03 +0000

    MOve mdraw and textview into separate dirs

---
+++ mdraw/mdraw.mk
@@ -0,0 +1,7 @@
+MDRAW_DIR = $(DRIVER_DIR)/mdraw
+
+VPATH += $(MDRAW_DIR)
+INCLUDES += -I$(MDRAW_DIR)
+
+SRC += mdraw.c
+
---
+++ textview/textview.mk
@@ -0,0 +1,7 @@
+TEXTVIEW_DIR = $(DRIVER_DIR)/textview
+
+VPATH += $(TEXTVIEW_DIR)
+INCLUDES += -I$(TEXTVIEW_DIR)
+
+SRC += textview.c
+
