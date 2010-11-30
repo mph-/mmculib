700693942bca1e3fa18fcb4d37f6909799478339
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-11-30 04:09:41 +0000

    Tidy FAT stuff

---
+++ fat_fs/fat_debug.h
@@ -0,0 +1,24 @@
+/** @file   fat_debug.h
+    @author Michael Hayes
+    @date   7 January 2009
+    @brief  FAT filesystem debugging routines.
+*/
+
+#ifndef FAT_DEBUG_H
+#define FAT_DEBUG_H
+
+#include "fat.h"
+
+void fat_debug_dir_dump (fat_t *fat, uint32_t dir_cluster);
+
+
+void fat_debug_rootdir_dump (fat_t *fat);
+
+
+void fat_debug_file (fat_t *fat, const char *filename);
+
+
+void fat_debug_partition (fat_t *fat);
+
+#endif
+
