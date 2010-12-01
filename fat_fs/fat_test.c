e56b101329541c0c4bb562ce3b5bea7e431224dc
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-12-01 00:02:54 +0000

    Add fat_test

---
+++ fat_fs/fat_test.c
@@ -0,0 +1,29 @@
+#include "fat_file.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/fcntl.h>
+
+int
+fat_test1 (fat_t *fat, int num)
+{
+    int i;
+
+    for (i = 0; i < num; i++)
+    {
+        uint8_t buffer[512];
+        char filename[32];
+        fat_file_t *file;
+
+        sprintf (filename, "test%d.tst", i);
+        memset (buffer, i, 512);
+
+        file = fat_open (fat, filename, O_CREAT | O_RDWR | O_TRUNC);
+        if (!file)
+            break;
+
+        fat_write (file, buffer, 512);
+        fat_close (file);
+    }
+    return i;
+}
