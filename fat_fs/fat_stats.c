cdd3dc9298780c17142627c0dd6366a08306a119
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-12-16 20:41:48 +0000

    Tidy

--- fat_fs/fat_stats.c
+++ fat_fs/fat_stats.c
@@ -13,8 +13,9 @@ void fat_stats (fat_t *fat, fat_stats_t *stats)
     fat_cluster_stats_t cluster_stats;
     uint32_t cluster_kB;
 
-    cluster_kB = fat->bytes_per_sector * fat->sectors_per_cluster / 1024;
+    cluster_kB = fat->bytes_per_cluster / 1024;
 
+    /* This can take a while on a large sdcard.  */
     fat_cluster_stats (fat, &cluster_stats);
     stats->free = cluster_stats.free * cluster_kB;
     stats->alloc = cluster_stats.alloc * cluster_kB;
