b9c32e338bd88c8e15eee5ff63abf9d3271c3f5b
Michael Hayes <michael.hayes@canterbury.ac.nz>
2013-05-21 08:40:33 +1200

    Rename macros

--- ir_sirc/ir_sirc_rx.c
+++ ir_sirc/ir_sirc_rx.c
@@ -32,22 +32,23 @@
 #include "ir_sirc.h"
 #include "delay.h"
 
-#ifndef IR_SIRC_DIT_PERIOD
-#define IR_SIRC_DIT_PERIOD 0.6e-3
+
+#ifndef IR_SIRC_RX_DIT_PERIOD
+#define IR_SIRC_RX_DIT_PERIOD 0.6e-3
 #endif
 
 
-#ifndef IR_SIRC_ACTIVE_STATE
-#define IR_SIRC_ACTIVE_STATE 0
+#ifndef IR_SIRC_RX_ACTIVE_STATE
+#define IR_SIRC_RX_ACTIVE_STATE 0
 #endif
 
 
-#define IR_SIRC_DELAY_US 10
-#define IR_SIRC_DIT_COUNT (1e6 * IR_SIRC_DIT_PERIOD / IR_SIRC_DELAY_US)
-#define IR_SIRC_START_COUNT_MAX ((int)(4.5 * IR_SIRC_DIT_COUNT))
-#define IR_SIRC_ONE_COUNT_MAX ((int)(2.5 * IR_SIRC_DIT_COUNT))
-#define IR_SIRC_ZERO_COUNT_MAX ((int)(1.25 * IR_SIRC_DIT_COUNT))
-#define IR_SIRC_BREAK_COUNT_MAX ((int)(1.5 * IR_SIRC_DIT_COUNT))
+#define IR_SIRC_RX_DELAY_US 10
+#define IR_SIRC_RX_DIT_COUNT (1e6 * IR_SIRC_RX_DIT_PERIOD / IR_SIRC_RX_DELAY_US)
+#define IR_SIRC_RX_START_COUNT_MAX ((int)(4.5 * IR_SIRC_RX_DIT_COUNT))
+#define IR_SIRC_RX_ONE_COUNT_MAX ((int)(2.5 * IR_SIRC_RX_DIT_COUNT))
+#define IR_SIRC_RX_ZERO_COUNT_MAX ((int)(1.25 * IR_SIRC_RX_DIT_COUNT))
+#define IR_SIRC_RX_BREAK_COUNT_MAX ((int)(1.5 * IR_SIRC_RX_DIT_COUNT))
 
 
 /** Return output state of IR receiver.
@@ -57,7 +58,7 @@ static inline uint8_t ir_sirc_rx_get (void)
     /* The output of most IR receivers are normally high but go low
        when modulated IR light is detected.  */
 
-    return pio_input_get (IR_SIRC_RX_PIO) == IR_SIRC_ACTIVE_STATE;
+    return pio_input_get (IR_SIRC_RX_PIO) == IR_SIRC_RX_ACTIVE_STATE;
 }
 
 
@@ -77,15 +78,15 @@ ir_sirc_ret_t ir_sirc_rx_read (uint8_t *pcommand, uint16_t *paddress)
 
     /* Check for start code; if not present return.  */
     if (!ir_rx_get ())
-        return IR_SIRC_NONE;
+        return IR_SIRC_RX_NONE;
 
     /* Wait for end of start code or timeout.  */
     for (count = 0; ir_rx_get (); count++)
     {
-        if (count >= IR_SIRC_START_COUNT_MAX)
-            return IR_SIRC_START_ERR;
+        if (count >= IR_SIRC_RX_START_COUNT_MAX)
+            return IR_SIRC_RX_START_ERR;
 
-        DELAY_US (IR_SIRC_DELAY_US);
+        DELAY_US (IR_SIRC_RX_DELAY_US);
     }
 
     /* We may have received a rogue short pulse but we may have been
@@ -103,25 +104,25 @@ ir_sirc_ret_t ir_sirc_rx_read (uint8_t *pcommand, uint16_t *paddress)
            detection of a false start code).  */
         for (count = 0; !ir_rx_get (); count++)
         {
-            if (count >= IR_SIRC_BREAK_COUNT_MAX)
-                return IR_SIRC_BREAK_ERR;
+            if (count >= IR_SIRC_RX_BREAK_COUNT_MAX)
+                return IR_SIRC_RX_BREAK_ERR;
 
-            DELAY_US (IR_SIRC_DELAY_US);
+            DELAY_US (IR_SIRC_RX_DELAY_US);
         }
 
         /* If there is another transmission in this slot we will get
            too small a value for count.  This is likely to trigger
            a bit error.  */
-        for (count = 0; count < IR_SIRC_ONE_COUNT_MAX
+        for (count = 0; count < IR_SIRC_RX_ONE_COUNT_MAX
                  && ir_rx_get (); count++)
         {
-            DELAY_US (IR_SIRC_DELAY_US);
+            DELAY_US (IR_SIRC_RX_DELAY_US);
         }
 
-        if (count >= IR_SIRC_ONE_COUNT_MAX)
+        if (count >= IR_SIRC_RX_ONE_COUNT_MAX)
             data_err = 1;
             
-        if (count >= IR_SIRC_ZERO_COUNT_MAX)
+        if (count >= IR_SIRC_RX_ZERO_COUNT_MAX)
             data |= 0x80;
     }
 
@@ -130,13 +131,13 @@ ir_sirc_ret_t ir_sirc_rx_read (uint8_t *pcommand, uint16_t *paddress)
 
     *pcommand = data & 0x7f;
     *paddress = data >> 7;
-    return data_err ? IR_SIRC_DATA_ERR : IR_SIRC_OK;
+    return data_err ? IR_SIRC_RX_DATA_ERR : IR_SIRC_RX_OK;
 }
 
 
 /** Initialise IR serial receiver driver.  */
 void ir_sirc_rx_init (void)
 {
-    /* Configure receiver PIO as input.  */
+    /* Configure IR receiver PIO as input.  */
     pio_config_set (IR_SIRC_RX_PIO, PIO_INPUT);
 }
