1b698b99531917d9d060a22cc134b91be8ad5178
Michael Hayes <michael.hayes@canterbury.ac.nz>
2020-01-31 09:43:24 +1300

    Add another pass

--- drivers.mk
+++ drivers.mk
@@ -1,9 +1,19 @@
 DRIVER_DIR = $(MMCULIB_DIR)
 
+
 include $(foreach driver, $(DRIVERS), $(DRIVER_DIR)/$(driver)/$(driver).mk)
+# Remove duplicates
+DRIVERS := $(sort $(DRIVERS))
+
+# Ideally should iterate until no new drivers added...
 
 # Perform second pass for drivers that depend on other drivers
 include $(foreach driver, $(DRIVERS), $(DRIVER_DIR)/$(driver)/$(driver).mk)
+DRIVERS := $(sort $(DRIVERS))
+
+# Perform third pass for drivers that depend on other drivers
+include $(foreach driver, $(DRIVERS), $(DRIVER_DIR)/$(driver)/$(driver).mk)
+DRIVERS := $(sort $(DRIVERS))
 
 VPATH += $(DRIVER_DIR)/utility $(DRIVER_DIR)
 
