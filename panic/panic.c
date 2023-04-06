a82e247257a5697ce3cb0df7b13ba48cb5b1687a
Harry Mander <harrymander96@gmail.com>
2023-04-06 15:06:55 +1200

    panic: fix undeclared symbol error due to undefined LED_ACTIVE macro

--- panic/panic.c
+++ panic/panic.c
@@ -6,9 +6,10 @@ panic (pio_t led_error_pio, unsigned int error_code)
 {
     unsigned int i;
 
-    pio_config_set (led_error_pio, PIO_OUTPUT_LOW);
-#ifndef LED_ACTIVE
+#ifdef LED_ACTIVE
     pio_output_set (led_error_pio, LED_ACTIVE);
+#else
+    pio_config_set (led_error_pio, PIO_OUTPUT_LOW);
 #endif
 
     while (1)
